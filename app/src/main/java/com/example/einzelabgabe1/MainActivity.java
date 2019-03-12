89d1a38f4a7927624017272e27e822a5f54e4c26
criedl1 <christoph.riedl1@gmail.com>
2019-03-12 11:29:28 +0100

    Test

--- app/src/main/java/com/example/einzelabgabe1/MainActivity.java
+++ app/src/main/java/com/example/einzelabgabe1/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    int test = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
